8d52e01c63da12b23a75c54ea8ca9862b9ea5c12
Gulimujyujyu ZHU <lucienzhu@gulimujyujyu.me>
2012-04-15 21:29:07 +0800

    initialize scripts for main.html

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created with PyCharm.
+ * User: xlzhu
+ * Date: 12-4-15
+ * Time: 下午9:21
+ * To change this template use File | Settings | File Templates.
+ */
+
+var Globals = {
+    "author": 'xlzhu',
+    "version_major": 0,
+    "version_minor": 1
+};
+
+$(document).ready(function(){
+    $(".container").append(Globals.author);
+});
