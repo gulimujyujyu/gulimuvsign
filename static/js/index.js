9db839620606ad2b7e67b0ddb350f324b317bfe3
Gulimujyujyu ZHU <lucienzhu@gulimujyujyu.me>
2012-04-15 21:52:13 +0800

    initialize map_widget to main.html

--- static/js/index.js
+++ static/js/index.js
@@ -9,9 +9,15 @@
 var Globals = {
     "author": 'xlzhu',
     "version_major": 0,
-    "version_minor": 1
+    "version_minor": 1,
+    "central_map_options": {
+        center: new google.maps.LatLng(-34.397, 150.644),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
 };
 
 $(document).ready(function(){
-    $(".container").append(Globals.author);
+    //initial google map
+    var map = new google.maps.Map(document.getElementById("central_map_canvas"), Globals.central_map_options);
 });
