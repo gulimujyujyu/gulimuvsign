426d8a273a0b8364423c36097641c025977c9732
hkxyli <hkxyli@gmail.com>
2012-04-16 21:36:07 +0800

    Add proximity_fetch for Search

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ from data import image_model
 from geocode import geocoder
 from Upload import upload
 
+from geo import geotypes
+import logging
+from django.utils import simplejson
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
@@ -34,6 +38,37 @@ class MainPage(webapp2.RequestHandler):
             result = None
 
         if result:
+            ###
+            lat = result['geometry']['location']['lat']
+            lon = result['geometry']['location']['lng']
+            center = geotypes.Point(float(lat),float(lon))
+            logging.info(center)
+
+            #location="%s,%s" % (geo_info.lat, geo_info.lon)
+            results = image_model.VSignImage.proximity_fetch(
+                image_model.VSignImage.all(),  # Rich query!
+                center,  # Or db.GeoPt
+                max_results=3,
+                max_distance=100000)  #m 80467==Within 50 miles.
+
+            logging.info(results.__getitem__(0))
+
+            public_attrs = image_model.VSignImage.public_attributes()
+
+            results_obj = [
+            _merge_dicts({
+                'lat': image_result.location.lat,
+                'lng': image_result.location.lon,
+                'user':image_result.user,
+                'image_key':image_result.image_key},
+                dict([(attr, getattr(result, attr))
+                for attr in public_attrs]))
+            for image_result in results]
+
+            logging.info(results_obj)
+            simplejson.dumps({'results': results_obj})
+
+            ###
             map_query = 'a'
         else:
             map_query = None
